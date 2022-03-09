9e230b8f05e0d35fe8b43c9198b789c94e1b7aa8
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-03-09 22:57:10 +0600

    commit 6 page

--- Lucky-Mate/js/main.js
+++ Lucky-Mate/js/main.js
@@ -2,4 +2,19 @@
 	"use strict";
 
     
+	
 }(jQuery));	
+
+
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("referralurl");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+}
