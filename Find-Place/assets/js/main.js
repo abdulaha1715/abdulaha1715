665060b5275b0a9092d651f8bc716883be1347c9
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-04-15 01:50:08 +0600

    Find-Place Updated

--- Find-Place/assets/js/main.js
+++ Find-Place/assets/js/main.js
@@ -5,9 +5,36 @@
         $(".menu-icon").click(function(){
             $(".menu-icon").toggleClass("active");
         });
+
         $(".menu-icon").click(function(){
             $(".sidebar").toggleClass("active");
         });
+
+
+        // gallery slider in single page 
+        $ (".anmipes-product-big-image").slick ({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            // arrows: true,
+            asNavFor: '.anmipes-thumb_imges',
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+        });
+
+        $ (".anmipes-thumb_imges").slick ({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            // arrows: true,
+            asNavFor: '.anmipes-product-big-image',
+            centerMode: false,
+            focusOnSelect: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+        });
+
+
     });
 
 }(jQuery));
