3690a666593c0bdc27f57b053d14bc78086ad100
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-04-14 21:50:08 +0600

    Slick and Slidebar responsive nav added

---
+++ Navigations/Slick-nav/js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+	"use strict";
+
+	//#main-slider
+	jQuery(document).ready(function($) {
+		
+		$("ul#navigation").slicknav({
+			prependTo: ".responsive-menu-wrap"
+
+		});
+	});
+
+	
+
+
+
+
+}(jQuery));
